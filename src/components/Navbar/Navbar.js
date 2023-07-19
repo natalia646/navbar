72839c1e9655b2daee8eaee461b227c0a3adf367
natalia646 <nataxash46@gmail.com>
2023-07-19 18:33:39 +0300

    work

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,36 +5,25 @@ import Burger from "../assets/burger.svg";
 import Close from "../assets/close.svg";
 
 const Navbar = () => {
+// const pages = ['Home', 'About', 'Skils', 'Sign-Up' ,'Contacts'];
+// const router = ["/", '/home', '/about', '/skils', '/signup' ,'contacts']
+
   const [isMobile, setIsMobile] = useState(false);
 
   return (
     <nav className="navbar">
       <h3 className="logo">Logo</h3>
-      <ul
-        className={isMobile ? "nav-links-mobile" : "nav-links"}
-        onClick={() => setIsMobile(false)}
-      >
-        <Link to="/" className="home">
-          Home
-        </Link>
-        <Link to="/about" className="about">
-          About
-        </Link>
-        <Link to="/skills" className="skills">
-          Skills
-        </Link>
-        <Link to="/signup" className="signup">
-          {" "}
-          Sign Up
-        </Link>
-        <Link to="/contacts" className="contacts">
-          Contacts
-        </Link>
+      <ul className={isMobile ? "nav-links-mobile" : "nav-links"}
+        onClick={() => setIsMobile(false)}>
+        <Link to="/" className="home">Home</Link>
+        <Link to="/about" className="about"> About </Link>
+        <Link to="/skills" className="skills">Skills</Link>
+        <Link to="/signup" className="signup">Sign Up</Link>
+        <Link to="/contacts" className="contacts">Contacts</Link> 
       </ul>
       <button
         className="mobile-menu-icon"
-        onClick={() => setIsMobile(!isMobile)}
-      >
+        onClick={() => setIsMobile(!isMobile)}>
         {isMobile ? (
           <img className="burger-icons" src={Close}></img>
         ) : (
